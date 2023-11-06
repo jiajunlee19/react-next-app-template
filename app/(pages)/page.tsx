432697f91c6dae59ec7597e9f04cb9bb0ae8dc9a
Jia Jun Lee <lee19@live.cn>
2023-11-06 20:04:37 +0800

    Add next/image placeholder

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
     return (
         <>
             <h1>Home</h1>
             <p>This is your home page!</p>
+            {/* <Image
+                src="/desktop.png"
+                width={1000}
+                height={760}
+                className="hidden md:block"
+                alt="This is a desktop image"
+            /> */}
         </>
     )
 };
