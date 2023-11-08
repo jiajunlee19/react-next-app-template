9f3bab17833e20ab9b2353148cf841d9870c5967
Jia Jun Lee <lee19@live.cn>
2023-11-08 15:56:28 +0800

    Add seedTray

--- app/_scripts/seed.ts
+++ app/_scripts/seed.ts
@@ -1,6 +1,6 @@
 // This is a seed script to load initial placeholder data to database, using "npm run seed"
 
-import { users, shipdocs, box_types, tray_types, boxes, trays, lots } from "@/app/_scripts/data_placeholder";
+import { users, shipdocs, lots, trays, boxes, tray_types, box_types } from "@/app/_scripts/data_placeholder";
 import { signUpSchema } from "@/app/_libs/zod_auth";
 import { parsedEnv } from "@/app/_libs/zod_env";
 import prisma from "@/prisma/prisma";
@@ -8,7 +8,7 @@ import sql from 'mssql';
 import { sqlConfig } from "@/app/_libs/sql_config";
 import { getErrorMessage } from "@/app/_libs/error_handler";
 import * as bcrypt from 'bcrypt';
-import { createShipdocSchema } from "@/app/_libs/zod_server";
+import { createLotSchema, createShipdocSchema, createTraySchema } from "@/app/_libs/zod_server";
 
 async function seedUser() {
 
@@ -72,7 +72,7 @@ async function seedShipdoc() {
             const result = await Promise.all(parsedForm.data.map( async (d) => {
                 pool.request()
                 .input('schema', sql.VarChar, 'packing')
-                .input('table', sql.VarChar, 'user')
+                .input('table', sql.VarChar, 'shipdoc')
                 .input('shipdoc_uid', sql.VarChar, d.shipdoc_uid)
                 .input('shipdoc_number', sql.Int, d.shipdoc_number)
                 .input('shipdoc_contact', sql.VarChar, d.shipdoc_contact)
@@ -91,9 +91,92 @@ async function seedShipdoc() {
 
 };
 
+async function seedLot() {
+
+    const parsedForm = createLotSchema.array().safeParse(lots);
+
+    if(!parsedForm.success) {
+        return { error: parsedForm.error.message.split('"message": "').pop()?.split('",')[0] }
+    }
+
+    try {
+        if (parsedEnv.DB_TYPE === 'PRISMA') {
+            const result = await Promise.all(parsedForm.data.map( async (d) => {
+                await prisma.lot.create({
+                    data: d,
+                })
+            }));
+        }
+        else {
+            let pool = await sql.connect(sqlConfig);
+            const result = await Promise.all(parsedForm.data.map( async (d) => {
+                pool.request()
+                .input('schema', sql.VarChar, 'packing')
+                .input('table', sql.VarChar, 'lot')
+                .input('lot_uid', sql.VarChar, d.lot_uid)
+                .input('tray_uid', sql.VarChar, d.tray_uid)
+                .input('lot_id', sql.VarChar, d.lot_id)
+                .input('lot_qty', sql.Int, d.lot_qty)
+                .input('lot_createdAt', sql.DateTime, d.lot_createdAt)
+                .input('lot_updatedAt', sql.DateTime, d.lot_updatedAt)
+                .query`INSERT INTO "@schema"."@table" 
+                        (lot_uid, tray_uid, lot_id, lot_qty, lot_createdAt, lot_updatedAt)
+                        VALUES (@lot_uid, @tray_uid, @lot_id, @lot_qty, @lot_createdAt, @lot_updatedAt);
+                `;
+            }));
+        }
+        return { success: `Successfully seed lots` }
+    } catch (err) {
+        return { error: getErrorMessage(err)}
+    }
+
+};
+
+async function seedTray() {
+
+    const parsedForm = createTraySchema.array().safeParse(trays);
+
+    if(!parsedForm.success) {
+        return { error: parsedForm.error.message.split('"message": "').pop()?.split('",')[0] }
+    }
+
+    try {
+        if (parsedEnv.DB_TYPE === 'PRISMA') {
+            const result = await Promise.all(parsedForm.data.map( async (d) => {
+                await prisma.tray.create({
+                    data: d,
+                })
+            }));
+        }
+        else {
+            let pool = await sql.connect(sqlConfig);
+            const result = await Promise.all(parsedForm.data.map( async (d) => {
+                pool.request()
+                .input('schema', sql.VarChar, 'packing')
+                .input('table', sql.VarChar, 'tray')
+                .input('tray_uid', sql.VarChar, d.tray_uid)
+                .input('box_uid', sql.Int, d.box_uid)
+                .input('tray_type_uid', sql.VarChar, d.tray_type_uid)
+                .input('tray_createdAt', sql.DateTime, d.tray_createdAt)
+                .input('tray_updatedAt', sql.DateTime, d.tray_updatedAt)
+                .query`INSERT INTO "@schema"."@table" 
+                        (tray_uid, box_uid, tray_type_uid, tray_createdAt, tray_updatedAt)
+                        VALUES (@tray_uid, @box_uid, @tray_type_uid, @tray_createdAt, @tray_updatedAt);
+                `;
+            }));
+        }
+        return { success: `Successfully seed trays` }
+    } catch (err) {
+        return { error: getErrorMessage(err)}
+    }
+
+};
+
 async function main() {
     console.log(await seedUser());
     console.log(await seedShipdoc());
+    console.log(await seedLot());
+    console.log(await seedTray);
 };
 
 // Run main
