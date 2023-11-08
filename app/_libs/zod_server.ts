24cdcfdf0cf66e28090d4897c0a9b83167eeb917
Jia Jun Lee <lee19@live.cn>
2023-11-08 15:32:21 +0800

    Optimize zod

--- app/_libs/zod_server.ts
+++ app/_libs/zod_server.ts
@@ -1,51 +1,80 @@
 import { z } from 'zod';
 
-export type TReadBoxSchema = z.infer<typeof readBoxSchema>;
-
-export const readBoxSchema = z.object({
-    box_uid: z.string().min(1).uuid().optional(),
-    box_type_uid: z.string().min(1).uuid().optional(),
-    box_part_number: z.string().length(10, {message: "Please input a valid part number!"}).includes("-", {message: "Please input a valid part number!"}).optional(),
-    box_max_tray: z.coerce.number().int().min(1).optional(),
-    box_createdAt: z.coerce.date().optional(),
-    box_updatedAt: z.coerce.date().optional(),
+export const createShipdocSchema = z.object({
+    shipdoc_uid: z.string().min(1).uuid(),
+    shipdoc_number: z.coerce.number().int().min(1),
+    shipdoc_contact: z.string().min(1),
+    shipdoc_createdAt: z.coerce.date(),
+    shipdoc_updatedAt: z.coerce.date(),
 });
+
+export type TReadShipdocSchema = z.infer<typeof readShipdocSchema>;
+
+export const readShipdocSchema = createShipdocSchema.partial();
+
+export const updateShipdocSchema = createShipdocSchema.omit({
+    shipdoc_createdAt: true,
+});
+
+export const deleteShipdocSchema = createShipdocSchema.pick({
+    shipdoc_uid: true,
+});
+
+
+
+export const createLotSchema = z.object({
+    lot_uid: z.string().min(1).uuid(),
+    tray_uid: z.string().min(1).uuid(),
+    lot_id: z.string().length(10, {message: "Please enter a valid lot!"}),
+    lot_qty: z.coerce.number().int().min(1),
+    lot_createdAt: z.coerce.date(),
+    lot_updatedAt: z.coerce.date(),
+});
+
+export type TReadLotSchema = z.infer<typeof readLotSchema>;
+
+export const readLotSchema = createLotSchema.partial();
+
+export const updateLotSchema = createLotSchema.omit({
+    lot_createdAt: true,
+});
+
+export const deleteLotSchema = createLotSchema.pick({
+    lot_uid: true,
+});
+
+
 
 export const createBoxSchema = z.object({
     box_uid: z.string().min(1).uuid(),
     box_type_uid: z.string().min(1).uuid(),
+    shipdoc_uid: z.string().min(1).uuid(),
     box_status: z.enum(['active', 'shipped']),
     box_createdAt: z.coerce.date(),
     box_updatedAt: z.coerce.date(),
 });
 
-export const updateBoxSchema = z.object({
-    box_uid: z.string().min(1).uuid(),
-    box_type_uid: z.string().min(1).uuid(),
-    box_status: z.enum(['active', 'shipped']),
-    box_updatedAt: z.coerce.date(),
+export type TReadBoxSchema = z.infer<typeof readBoxSchema>;
+
+export const readBoxSchema = createBoxSchema.extend({
+    box_part_number: z.string().length(10, {message: "Please input a valid part number!"}).includes("-", {message: "Please input a valid part number!"}),
+}).partial();
+
+export const updateBoxSchema = createBoxSchema.omit({
+    box_createdAt: true,
 });
 
-export const deleteBoxSchema = z.object({
-    box_uid: z.string().min(1).uuid(),
+export const deleteBoxSchema = createBoxSchema.pick({
+    box_uid: true,
 });
 
-export const shipBoxSchema = z.object({
-    box_uid: z.string().min(1).uuid(),
-    box_status: z.enum(['active', 'shipped']),
-    box_updatedAt: z.coerce.date(),
+export const shipBoxSchema = createBoxSchema.pick({
+    box_uid: true,
+    box_status: true,
+    box_updatedAt: true,
 });
 
 
-export type TReadTraySchema = z.infer<typeof readTraySchema>;
-
-export const readTraySchema = z.object({
-    tray_uid: z.string().min(1).uuid().optional(),
-    box_uid: z.string().min(1).uuid().optional(),
-    tray_type_uid: z.string().min(1).uuid().optional(),
-    tray_createdAt: z.coerce.date().optional(),
-    tray_updatedAt: z.coerce.date().optional(),
-});
 
 export const createTraySchema = z.object({
     tray_uid: z.string().min(1).uuid(),
@@ -55,123 +84,19 @@ export const createTraySchema = z.object({
     tray_updatedAt: z.coerce.date(),
 });
 
-export const updateTraySchema = z.object({
-    tray_uid: z.string().min(1).uuid(),
-    box_uid: z.string().min(1).uuid(),
-    tray_type_uid: z.string().min(1).uuid(),
-    tray_updatedAt: z.coerce.date(),
+export type TReadTraySchema = z.infer<typeof readTraySchema>;
+
+export const readTraySchema = createTraySchema.partial();
+
+export const updateTraySchema = createTraySchema.omit({
+    tray_createdAt: true,
 });
 
-export const deleteTraySchema = z.object({
-    tray_uid: z.string().min(1).uuid(),
-});
-
-
-export type TReadLotSchema = z.infer<typeof readLotSchema>;
-
-export const readLotSchema = z.object({
-    lot_uid: z.string().min(1).uuid().optional(),
-    tray_uid: z.string().min(1).uuid().optional(),
-    shipdoc_uid: z.string().min(1).uuid().optional(),
-    lot_id: z.string().length(10, {message: "Please enter a valid lot!"}).optional(),
-    lot_qty: z.coerce.number().int().min(1).optional(),
-    lot_createdAt: z.coerce.date().optional(),
-    lot_updatedAt: z.coerce.date().optional(),
-});
-
-export const createLotSchema = z.object({
-    lot_uid: z.string().min(1).uuid(),
-    tray_uid: z.string().min(1).uuid(),
-    shipdoc_uid: z.string().min(1).uuid(),
-    lot_id: z.string().length(10, {message: "Please enter a valid lot!"}),
-    lot_qty: z.coerce.number().int().min(1),
-    lot_createdAt: z.coerce.date(),
-    lot_updatedAt: z.coerce.date(),
-});
-
-export const updateLotSchema = z.object({
-    lot_uid: z.string().min(1).uuid(),
-    tray_uid: z.string().min(1).uuid(),
-    shipdoc_uid: z.string().min(1).uuid(),
-    lot_id: z.string().length(10, {message: "Please enter a valid lot!"}),
-    lot_qty: z.coerce.number().int().min(1),
-    lot_updatedAt: z.coerce.date(),
-});
-
-export const deleteLotSchema = z.object({
-    lot_uid: z.string().min(1).uuid(),
+export const deleteTraySchema = createTraySchema.pick({
+    tray_uid: true,
 });
 
 
-export type TReadShipdocSchema = z.infer<typeof readShipdocSchema>;
-
-export const readShipdocSchema = z.object({
-    shipdoc_uid: z.string().min(1).uuid().optional(),
-    shipdoc_number: z.coerce.number().int().min(1).optional(),
-    shipdoc_contact: z.string().min(1).optional(),
-    shipdoc_createdAt: z.coerce.date().optional(),
-    shipdoc_updatedAt: z.coerce.date().optional(),
-});
-
-export const createShipdocSchema = z.object({
-    shipdoc_uid: z.string().min(1).uuid(),
-    shipdoc_number: z.coerce.number().int().min(1),
-    shipdoc_contact: z.string().min(1),
-    shipdoc_createdAt: z.coerce.date(),
-    shipdoc_updatedAt: z.coerce.date(),
-});
-
-export const updateShipdocSchema = z.object({
-    shipdoc_uid: z.string().min(1).uuid(),
-    shipdoc_number: z.coerce.number().int().min(1),
-    shipdoc_contact: z.string().min(1),
-    shipdoc_updatedAt: z.coerce.date(),
-});
-
-export const deleteShipdocSchema = z.object({
-    shipdoc_uid: z.string().min(1).uuid(),
-});
-
-
-export type TReadBoxTypeSchema = z.infer<typeof readBoxTypeSchema>;
-
-export const readBoxTypeSchema = z.object({
-    box_type_uid: z.string().min(1).uuid().optional(),
-    box_part_number: z.string().length(10, {message: "Please input a valid part number!"}).includes("-", {message: "Please input a valid part number!"}).optional(),
-    box_max_tray: z.coerce.number().int().min(1).optional(),
-    box_type_createdAt: z.coerce.date().optional(),
-    box_type_updatedAt: z.coerce.date().optional(),
-});
-
-export const createBoxTypeSchema = z.object({
-    box_type_uid: z.string().min(1).uuid(),
-    box_part_number: z.string().length(10, {message: "Please input a valid part number!"}).includes("-", {message: "Please input a valid part number!"}),
-    box_max_tray: z.coerce.number().int().min(1),
-    box_type_createdAt: z.coerce.date(),
-    box_type_updatedAt: z.coerce.date(),
-});
-
-export const updateBoxTypeSchema = z.object({
-    box_type_uid: z.string().min(1).uuid(),
-    box_part_number: z.string().length(10, {message: "Please input a valid part number!"}).includes("-", {message: "Please input a valid part number!"}),
-    box_max_tray: z.coerce.number().int().min(1),
-    box_type_updatedAt: z.coerce.date(),
-});
-
-export const deleteBoxTypeSchema = z.object({
-    box_type_uid: z.string().min(1).uuid(),
-});
-
-
-export type TReadTrayTypeSchema = z.infer<typeof readTrayTypeSchema>;
-
-export const readTrayTypeSchema = z.object({
-    tray_type_uid: z.string().min(1).uuid().optional(),
-    tray_part_number: z.string().length(10, {message: "Please input a valid part number!"}).includes("-", {message: "Please input a valid part number!"}).optional(),
-    tray_max_drive: z.coerce.number().int().min(1).optional(),
-    tray_type_createdAt: z.coerce.date().optional(),
-    tray_type_updatedAt: z.coerce.date().optional(),
-});
 
 export const createTrayTypeSchema = z.object({
     tray_type_uid: z.string().min(1).uuid(),
@@ -181,13 +106,36 @@ export const createTrayTypeSchema = z.object({
     tray_type_updatedAt: z.coerce.date(),
 });
 
-export const updateTrayTypeSchema = z.object({
-    tray_type_uid: z.string().min(1).uuid(),
-    tray_part_number: z.string().length(10, {message: "Please input a valid part number!"}).includes("-", {message: "Please input a valid part number!"}),
-    tray_max_drive: z.coerce.number().int().min(1),
-    tray_type_updatedAt: z.coerce.date(),
+export type TReadTrayTypeSchema = z.infer<typeof readTrayTypeSchema>;
+
+export const readTrayTypeSchema = createTrayTypeSchema.partial();
+
+export const updateTrayTypeSchema = createTrayTypeSchema.omit({
+    tray_type_createdAt: true,
 });
 
-export const deleteTrayTypeSchema = z.object({
-    tray_type_uid: z.string().min(1).uuid(),
+export const deleteTrayTypeSchema = createTrayTypeSchema.pick({
+    tray_type_uid: true,
+});
+
+
+
+export const createBoxTypeSchema = z.object({
+    box_type_uid: z.string().min(1).uuid(),
+    box_part_number: z.string().length(10, {message: "Please input a valid part number!"}).includes("-", {message: "Please input a valid part number!"}),
+    box_max_tray: z.coerce.number().int().min(1),
+    box_type_createdAt: z.coerce.date(),
+    box_type_updatedAt: z.coerce.date(),
+});
+
+export type TReadBoxTypeSchema = z.infer<typeof readBoxTypeSchema>;
+
+export const readBoxTypeSchema = createBoxTypeSchema.partial();
+
+export const updateBoxTypeSchema = createBoxTypeSchema.omit({
+    box_type_createdAt: true,
+});
+
+export const deleteBoxTypeSchema = createBoxTypeSchema.pick({
+    box_type_uid: true,
 });
