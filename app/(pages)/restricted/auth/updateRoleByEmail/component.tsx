45cdd46498f9cb132f8ddef4fc57faf326556020
Jia Jun Lee <lee19@live.cn>
2023-11-12 15:32:01 +0800

    Remove deleteButton here

--- app/(pages)/restricted/auth/updateRoleByEmail/component.tsx
+++ app/(pages)/restricted/auth/updateRoleByEmail/component.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { updateUser, deleteUser } from "@/app/_actions/auth";
+import { updateRole } from "@/app/_actions/auth";
 import SubmitButton from "@/app/_components/basic/button_submit";
-import { TReadUserWithoutPassSchema } from "@/app/_libs/zod_auth";
-import { signOut, useSession } from "next-auth/react";
+import FormWithoutState from "@/app/_components/basic/form_without_state";
+import { type TReadUserWithoutPassSchema } from "@/app/_libs/zod_auth";
+import { useSession } from "next-auth/react";
 import { redirect, useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useRef } from "react";
 import { toast } from "react-hot-toast";
@@ -57,20 +58,11 @@ export default function UpdateRoleComponent( {user}: TUpdateRoleComponentProps )
         return; //proceed to submit form
     };
 
-    const handleDeleteClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        const input = window.prompt("Are you sure to delete this user?\nIf yes, enter 'delete' to confirm, this action cannot be undone.")
-        if (input?.toLowerCase() !== 'delete') {
-            e.preventDefault();
-            return;
-        }
-    };
-
     return (
         <>
             <h1>Update Role</h1>
-            
             <form ref={formRef} action={ async (formData) => {
-                        const result = await updateUser(formData);
+                        const result = await updateRole(formData);
                         if (result?.error && result?.message) {
                             toast.error(result.message);
                         }
@@ -80,39 +72,17 @@ export default function UpdateRoleComponent( {user}: TUpdateRoleComponentProps )
                         formRef.current?.reset();
                     }
                 }>
-                <input name="user_uid" type="text" defaultValue={user?.user_uid || session.user.user_uid} readOnly formNoValidate />
+                <input name="user_uid" type="text" defaultValue={user?.user_uid || session.user.user_uid} hidden formNoValidate />
                 <label htmlFor="email">Email: </label>
                 <input name="email" type="email" placeholder="Enter your email" defaultValue={searchParams.get("email")?.toString() || session.user.email} onBlur={handleEmailChange} onChange={useDebouncedCallback(handleEmailChange, 3000)} required formNoValidate />
                 <label htmlFor="role">Role: </label>
-                <select name="role" defaultValue={user?.role || session.user.role } required>
+                <select name="role" defaultValue="user" required>
                     {["user", "admin", "boss"].map((role) => {
-                        if (role === (user?.role || session.user.role) ) {
-                            return <option key={role} defaultValue={role}>{role}</option>
-                        }
-                        else {
-                            return <option key={role} value={role}>{role}</option>
-                        }
+                        return <option key={role} value={role}>{role}</option>
                     })}
                 </select>
                 <SubmitButton buttonClass="btn-ok w-40 mr-4 mt-4" buttonTitle="Update" onButtonClick={handleUpdateClick} submitingButtonTitle="Updating" />
                 <button type="button" className="btn-cancel w-40 mr-4 mt-4" onClick={() => formRef.current?.reset()}>Reset</button>
-            </form>
-
-            <form action={ async () => {
-                        const result = await deleteUser(searchParams.get("email")?.toString() || session.user.email);
-                        if (result?.error && result?.message) {
-                            toast.error(result.message);
-                        }
-                        else if (result?.message) {
-                            toast.success(result.message);
-                            await signOut({
-                                redirect: true,
-                                callbackUrl: "/",
-                            });
-                        }
-                    }
-                }>
-                <SubmitButton buttonClass="btn-cancel w-80 mr-4 mt-8" buttonTitle={"Delete " + searchParams.get("email")?.toString() || session.user.email} onButtonClick={handleDeleteClick} submitingButtonTitle="Deleting" />
             </form>
         </>
     );
