f67578647810952a2e6bbae8431ba19d5c18b204
Jia Jun Lee <lee19@live.cn>
2023-11-12 14:34:39 +0800

    Add protectedPage

--- app/(pages)/protected/page.tsx
+++ app/(pages)/protected/page.tsx
@@ -7,6 +7,17 @@ export const metadata: Metadata = {
 
 export default function ProtectedPage() {
     return (
-        <h1>This is a protected page!</h1>
+        <>
+            <h1>Protected Page for Admin</h1>
+            {/* <nav>
+                {authNavLinks.map((link) => {
+                    return (
+                        <li className="text-black dark:text-white" key={link.name}>
+                            <Link href={link.href}>{link.name}</Link>
+                        </li>
+                    )
+                })}
+            </nav> */}
+        </>
     );
 };
