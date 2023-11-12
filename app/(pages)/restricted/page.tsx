a314e768d25739dc154ae9a1aa3443abded8d822
Jia Jun Lee <lee19@live.cn>
2023-11-12 12:41:14 +0800

    Add restricted main page

--- app/(pages)/restricted/page.tsx
+++ app/(pages)/restricted/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,7 +7,25 @@ export const metadata: Metadata = {
 };
 
 export default function RestrictedPage() {
+
+    const authNavLinks = [
+        { name: "Manage User", href: "/restricted/auth/user" },
+        { name: "Update User Role by Email", href: "/restricted/updateRole" },
+    ];
+
     return (
-        <h1>This is a restricted page!</h1>
+        <>
+            <h1>Restricted Page for Boss</h1>
+            <h3>Auth</h3>
+            <nav>
+                {authNavLinks.map((link) => {
+                    return (
+                        <li key={link.name}>
+                            <Link href={link.href}>{link.name}</Link>
+                        </li>
+                    )
+                })}
+            </nav>
+        </>
     );
 };
