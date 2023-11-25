2d67bfce5d035fa07356d693a495caafdeaaf1e5
Jia Jun Lee <lee19@live.cn>
2023-11-25 14:41:55 +0800

    Update to inline-flex

--- app/_components/basic/breadcrumbs.tsx
+++ app/_components/basic/breadcrumbs.tsx
@@ -10,16 +10,14 @@ type Breadcrumb = {
 export default function Breadcrumbs( { breadcrumbs }: { breadcrumbs: Breadcrumb[] } ) {
     return (
         <nav aria-label="Breadcrumb" className="my-6 mx-[2%] block">
-            <ol className="flex items-center">
                 {breadcrumbs.map((breadcrumb, index) => (
-                    <li key={breadcrumb.href} aria-current={breadcrumb.active}>
+                    <li className="inline-flex flex-nowrap items-center" key={breadcrumb.href} aria-current={breadcrumb.active}>
                         <Link className={twMerge("no-underline", breadcrumb.active && "pointer-events-none underline font-semibold text-purple-500 dark:text-purple-200")} href={breadcrumb.href}>{breadcrumb.label}</Link>
                         {index < breadcrumbs.length -1 ?
                         <span className="mx-3 inline-block">/</span>
                          : null}
                     </li>
                 ))}
-            </ol>
         </nav>
     );
 };
