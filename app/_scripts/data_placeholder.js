637bb87f52fee341f3e4343b3289518e0fa7f9a4
Jia Jun Lee <lee19@live.cn>
2024-11-05 19:17:32 +0800

    [refactor] Migrate from bcrypt to bcryptjs

--- app/_scripts/data_placeholder.js
+++ app/_scripts/data_placeholder.js
@@ -1,12 +1,12 @@
 // This file contains placeholder data that you'll used for dev only, you can seed this into database using "npm run seed"
 
-import { hash } from "bcrypt";
+import bcrypt from 'bcryptjs';
 
 export const users = [
   {
     user_uid: '1493ac72-2669-5f17-a7c2-b78dca0f0344',
     email: 'jiajunlee@email.com',
-    password: await hash('12345678', 10),
+    password: await bcrypt.hash('12345678', 10),
     role: 'boss',
     user_created_dt: new Date(),
     user_updated_dt: new Date()
