f1cc91a5b97a39011a5a78305773a62aef15750d
Jia Jun Lee <lee19@live.cn>
2024-11-17 21:05:28 +0800

    [feat] Use global client

--- app/_libs/ldap.ts
+++ app/_libs/ldap.ts
@@ -1,10 +1,22 @@
 import { parsedEnv } from '@/app/_libs/zod_env';
 import ldap from 'ldapjs';
 
-console.log('creating client')
-const ldap_client = ldap.createClient({
-    // ldap://localhost:389/ou=company,dc=company,dc=com
-    url: parsedEnv.LDAP_URL,
-});
-
-export default ldap_client;
+const ldapClientSingleton = () => {
+    console.log('creating client')
+    return ldap.createClient({
+        // ldap://localhost:389/ou=company,dc=company,dc=com
+        url: parsedEnv.LDAP_URL,
+    });
+  }
+  
+  type ldapClientSingleton = ReturnType<typeof ldapClientSingleton>
+  
+  const globalForLDAP = globalThis as unknown as {
+    ldap_client: ldapClientSingleton | undefined
+  }
+  
+  const ldap_client = globalForLDAP.ldap_client ?? ldapClientSingleton()
+  
+  export default ldap_client
+  
+  if (process.env.NODE_ENV !== 'production') globalForLDAP.ldap_client = ldap_client
