871519a6dfd09b742b21f5085c86ea0ff73c77cb
Jia Jun Lee <lee19@live.cn>
2024-11-17 18:52:12 +0800

    [chore] Add string constraint as max(50) to align with column varchar(50)

--- app/_libs/zod_server.ts
+++ app/_libs/zod_server.ts
@@ -8,8 +8,8 @@ export const querySchema = z.string().min(1).optional().catch(undefined);
 
 export const createShipdocSchema = z.object({
     shipdoc_uid: z.string().toLowerCase().min(1).uuid(),
-    shipdoc_number: z.string().toUpperCase().min(1),
-    shipdoc_contact: z.string().toUpperCase().min(1),
+    shipdoc_number: z.string().toUpperCase().min(1).max(50),
+    shipdoc_contact: z.string().toUpperCase().min(1).max(50),
     shipdoc_created_dt: z.coerce.date(),
     shipdoc_updated_dt: z.coerce.date(),
 });
@@ -72,8 +72,8 @@ export const readBoxSchema = createBoxSchema.extend({
     box_part_number: z.string().toUpperCase().length(10, {message: "Please input a valid part number!"}).includes("-", {message: "Please input a valid part number!"}),
     box_max_tray: z.coerce.number().int().min(1),
     box_current_tray: z.coerce.number().int().min(0),
-    shipdoc_number: z.string().toUpperCase().min(1),
-    shipdoc_contact: z.string().toUpperCase().min(1),
+    shipdoc_number: z.string().toUpperCase().min(1).max(50),
+    shipdoc_contact: z.string().toUpperCase().min(1).max(50),
 }).partial();
 
 export const updateBoxSchema = createBoxSchema.pick({
