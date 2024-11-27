2d63d0361910707a2d3ede98de3f2da7dc9660b9
Jia Jun Lee <lee19@live.cn>
2024-11-27 18:48:21 +0800

    [feat] Add user_uid as primary key

--- T-SQL/create.sql
+++ T-SQL/create.sql
@@ -12,7 +12,9 @@ CREATE TABLE "packing"."user" (
     password VARCHAR(100) NOT NULL,
     role VARCHAR(50) NOT NULL,
     user_created_dt DATETIME NOT NULL,
-    user_updated_dt DATETIME NOT NULL
+    user_updated_dt DATETIME NOT NULL,
+
+    CONSTRAINT pk_user_uid PRIMARY KEY CLUSTERED (user_uid)
 )
 
 CREATE TABLE "packing"."box_type" (
