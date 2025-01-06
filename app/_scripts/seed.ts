efc79f4a593de4697b328f4c7130b2e27191f4bf
Jia Jun Lee <lee19@live.cn>
2025-01-06 15:11:28 +0800

    [fix] Update schema and table in mssql query string

--- app/_scripts/seed.ts
+++ app/_scripts/seed.ts
@@ -10,8 +10,6 @@ import { getErrorMessage } from "@/app/_libs/error_handler";
 import bcrypt from 'bcryptjs';
 import { createTypeSchema } from "@/app/_libs/zod_server";
 
-const DB_SCHEMA = parsedEnv.DB_SCHEMA;
-
 async function seedUser() {
 
     const parsedForm = signUpSchema.array().safeParse(users);
@@ -38,7 +36,7 @@ async function seedUser() {
                 .input('role', sql.VarChar, d.role)
                 .input('user_created_dt', sql.DateTime, d.user_created_dt)
                 .input('user_updated_dt', sql.DateTime, d.user_updated_dt)
-                .query`INSERT INTO "${DB_SCHEMA}"."user" 
+                .query`INSERT INTO [template].[user] 
                         (user_uid, username, password, role, user_created_dt, user_updated_dt)
                         VALUES (@user_uid, @username, @password, @role, @user_created_dt, @user_updated_dt);
                 `;
@@ -75,7 +73,7 @@ async function seedType() {
                 .input('type', sql.VarChar, d.type)
                 .input('type_created_dt', sql.DateTime, d.type_created_dt)
                 .input('type_updated_dt', sql.DateTime, d.type_updated_dt)
-                .query`INSERT INTO "${DB_SCHEMA}"."type" 
+                .query`INSERT INTO [template].[type] 
                         (type_uid, type, type_created_dt, type_updated_dt)
                         VALUES (@type_uid, @type, @type_created_dt, @type_updated_dt);
                 `;
